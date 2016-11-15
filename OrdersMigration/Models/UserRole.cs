3ad90fbfdb6c5811eea09595b7ae8548d5e1adc0
gsarmiento16 <guillermosarmiento@gmail.com>
2016-11-14 20:23:55 -0400

    UserRole

---
+++ OrdersMigration/Models/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersMigration.Models
+{
+    public partial class UserRole
+    {
+        public long Id { get; set; }
+        public string Ext_Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
